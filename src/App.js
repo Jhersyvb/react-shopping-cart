d2888f8ed4b5579c5e2f8f553aed43a0f062b593
Jhersy <jhersyvb@gmail.com>
2020-10-23 20:54:01 -0500

    Part 08- Checkout Form (Handle Forms)

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ class App extends React.Component {
     super()
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem('cartItems')
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        : [],
       size: '',
       sort: '',
     }
+  }
+
+  createOrder = order => {
+    alert('Need to save order for ' + order.name)
   }
 
   removeFromCart = product => {
@@ -20,6 +26,10 @@ class App extends React.Component {
     this.setState({
       cartItems: cartItems.filter(x => x._id !== product._id),
     })
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(cartItems.filter(x => x._id !== product._id))
+    )
   }
 
   addToCart = product => {
@@ -35,6 +45,7 @@ class App extends React.Component {
       cartItems.push({ ...product, count: 1 })
     }
     this.setState({ cartItems })
+    localStorage.setItem('cartItems', JSON.stringify(cartItems))
   }
 
   filterProducts = event => {
